97cdb4f12f085c5a29e23db82c5b9ebe5ec01e2b
carlosandregit <66290422+carlosandregit@users.noreply.github.com>
2022-05-27 11:42:11 -0300

    Estrutura de repetições WHILE e DO WHILE

---
+++ aula13/comerPizza.js
@@ -0,0 +1,7 @@
+let contrador = 1
+while(contrador <= 8){
+
+    console.log('pegou ' + contrador + 'fatias')    
+    contrador++
+}
+console.log('vixe, acabou a pizza')
---
+++ aula13/comerpizzaDoWhile.js
@@ -0,0 +1,7 @@
+let contrador = 1
+do{
+
+    console.log('pegou ' + contrador + 'fatias')    
+    contrador++
+}while(contrador <= 8)
+console.log('vixe, acabou a pizza')
